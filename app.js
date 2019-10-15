0d17ca73b14b6a9ffe62ab62e60aeb25a8caf82b
chad svastisalee <swasgrafx@gmail.com>
2019-10-15 18:22:10 -0500

    Import session and passport packages

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var app = express();
 
 var config = require('./config.dev');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var passport = require('passport');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
